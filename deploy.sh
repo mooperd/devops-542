c46c0f05e8b443506e03fb47df4706775853ac9d
Andrew Holway <andrew.holway@gmail.com>
2021-05-11 18:33:17 +0200

    third commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 # ┌───────────────────────────────────────────────┐
 # │                AWS LOAD BALANCER              │
@@ -22,7 +22,12 @@ export DATABASE_VPC=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=Datab
 # If DATABASE_VPC is empty then the database VPC does not exist. 
 if [ -z "$DATABASE_VPC" ]
     then    
-        echo "Database VPC not detected. Did you deploy one? It should be tagged - Name: Database"
+        echo "Database VPC not detected. Did you deploy one? It should be tagged - Name: Database. I'm going to create one now."
+        export DATABASE_VPC=$(aws ec2 create-vpc --cidr-block 10.141.0.0/16 --tag-specifications 'ResourceType=vpc, Tags=[{Key=Name,Value=Database}]' --output=text --query 'Vpc.[VpcId]')
+        export DATABASE_SN_1=$(aws ec2 create-subnet --vpc-id $DATABASE_VPC --cidr-block 10.141.10.0/24 --output text --query 'Subnet.[SubnetId]')
+        export DATABASE_SN_2=$(aws ec2 create-subnet --vpc-id $DATABASE_VPC --cidr-block 10.141.20.0/24 --output text --query 'Subnet.[SubnetId]')
+        export DATABASE_SN_3=$(aws ec2 create-subnet --vpc-id $DATABASE_VPC --cidr-block 10.141.30.0/24 --output text --query 'Subnet.[SubnetId]')
+        echo "vpc $DATABASE_VPC was created with subnets: $DATABASE_SN_1, $DATABASE_SN_2, $DATABASE_SN_3"
     else
         echo "Database VPC $DATABASE_VPC discovered. We are going to assume that there is Databases in there."
 fi
@@ -31,14 +36,21 @@ fi
 cat clusterConfig.yaml | envsubst > $CLUSTER_NAME.yaml
 
 # This creates our new kubernetes cluster and associated resources. 
-# eksctl create cluster -f $CLUSTER_NAME.yaml
+eksctl create cluster -f $CLUSTER_NAME.yaml || echo "Cluster $CLUSTER_NAME probably already exists"
+#if ! (( eksctl create cluster -f $CLUSTER_NAME.yaml ))
+#    then
+#        echo "Cluster $CLUSTER_NAME already exists"
+#    else
+#        echo "Cluster $CLUSTER_NAME was created"
+#fi
 
 # Grab the VPC with the Kubernetes in it.
 export K8S_VPC=$(aws ec2 describe-vpcs --filters Name=tag:alpha.eksctl.io/cluster-name,Values=$CLUSTER_NAME --output=text --query 'Vpcs[].[VpcId]')
 
 # Deploy an ingress controller. Along with the ingress controller will be deployed a Kubernetes service of Type: LoadBalancer.
 # This will create a public facing AWS load balancer automatically assigned to the cluster. 
-# We have to wait for a while before the public IP address becomes available.  
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.46.0/deploy/static/provider/aws/deploy.yaml
 
-
+# Find the public endpoint of our kubernetes cluster.
+export INGRESS_SERVICE_EXTERNAL=$(kubectl get service ingress-nginx-controller -n ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+echo "public endpoint of our kubernetes cluster is $INGRESS_SERVICE_EXTERNAL. You probably need to do something with that."
